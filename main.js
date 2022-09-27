c64b5ab614cb7101100898da8cae85c4e1e16638
carol castrillon <you@example.com>
2022-09-27 13:20:53 -0300

    clase de los ciclos

---
+++ main.js
@@ -0,0 +1,19 @@
+let n = 1
+do {
+    console.log(n)
+    n = n + 1 // n++
+} while (n <= 10)
+
+let n2 = 10
+do {
+    console.log(n2)
+    n2 = n2 - 1 // n--
+} while (n2 >= 1)
+
+const name="carol"
+n= name.length-1
+do {
+    console.log(name[n])
+    n=n-1
+}while(n>=0)
+
